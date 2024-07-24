4bb3f121093db621739303fd1e6f91d3327a8514
nishi240masa <masa2401106@gmail.com>
2024-07-24 10:07:17 +0900

    chore: Add Vite configuration files and update project dependencies

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // port番号を指定
+    port: 3000,
+    host: true,
+  }
+})
