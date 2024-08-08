9b3afcac393367da29d9097c9d690acdeb76cddb
nishi240masa <masa2401106@gmail.com>
2024-08-08 10:57:26 +0900

    Update:Prettier,configuration add:Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Home from './pages/Home';
 
-const App = () => <div />;
+const App = () => (
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/participant" element={<NewBlog />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NoMatch />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
