482fce399d826d8f42f01c4296ef676856b860cf
watagassa <k23095kk@aitech.ac.jp>
2024-08-10 11:21:56 +0900

    OrderNumber作成

---
+++ src/components/OrderNumber.tsx
@@ -0,0 +1,19 @@
+import { Card, Grid } from '@mui/material';
+import { grey } from '@mui/material/colors';
+
+const OrderNumber = () => (
+  <div>
+    <Grid container 
+    // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+    >
+      <Card sx={{
+        backgroundColor:grey[300],
+        fontSize: 40,
+        minWidth: 160,
+        
+      }}>185</Card>
+      
+    </Grid>
+  </div>
+);
+export default OrderNumber;
