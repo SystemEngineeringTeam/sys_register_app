d6533878b7605202bd4d84a809015df09e71cf2c
tanaka0005 <tanaka00005.com>
2024-08-10 16:15:44 +0900

    update:スクロールしても常に画面下に移るようにした。

--- src/components/OrderWaitPeople.tsx
+++ src/components/OrderWaitPeople.tsx
@@ -10,12 +10,10 @@ const OrderWaitPeople = ({orders}:OrderWaitPeopleProps) => {
   return (
     <div>
       <Box sx={{ 
-        position: 'absolute',
+        position: 'fixed', //下に固定する
         bottom:'0px',
         right: '0',   //横全体を写すため
         left: '0',  //横全体を写すため
-
-
         background:'#c8c8c8',
         textAlign: 'center',
         fontSize: '60px',
