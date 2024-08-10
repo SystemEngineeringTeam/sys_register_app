af27ead722a3f7706beca37bda77806c80bfe952
nishi240masa <masa2401106@gmail.com>
2024-08-10 22:31:09 +0900

    update:ProductOrderCardをレスポンシブデザインに対応させた

--- src/components/ProductOrderCard.tsx
+++ src/components/ProductOrderCard.tsx
@@ -9,26 +9,34 @@ const ProductOrderCard = () => {
   return (
     <div>
       <Box>
-        <Card variant="outlined" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <CardContent sx={{ display: 'flex', flexGrow:1 }}>
-            <Typography variant="h3">ねぎま</Typography>
-            <Typography variant="h3">4</Typography>
+        <Card variant="outlined" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+          <CardContent sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+            <Typography variant="h4">ねぎま</Typography>
+            <Typography variant="h4">X 4</Typography>
           </CardContent>
 
-          <Grid container spacing={1} >
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ m:2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Grid container spacing={2} >
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               <ItemOptions />
             </Grid>
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <ItemOptions  />
-            </Grid>
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               <ItemOptions />
             </Grid>
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <ItemOptions />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <ItemOptions />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <ItemOptions />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               <ItemOptions />
             </Grid>
           </Grid>
+          </Box>
         </Card>
       </Box>
     </div>
