d1eda9a20298a08b43d43f3d990dd9b772100d36
tanaka0005 <tanaka00005.com>
2024-08-10 16:27:43 +0900

    update:配列数を増やした

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -6,7 +6,7 @@ import OrderWaitPeople from '../components/OrderWaitPeople';
 
 
 const Order = ( ) => {
-    const orders = [1,2,3,4,4,5,4,231,3245,324,332,344];
+    const orders = [1,2,3,4,4,5,4,231,3245,324,332,344,223,421,324,321,123,242,234,231,324,23,4,234,443,244,232];
   return (
     <div>
       <NumberButtonBox orders={orders}/>
