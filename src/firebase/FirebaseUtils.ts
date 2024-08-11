cdb71807e96a679640541ec853bbedf0b6f55680
nishi240masa <masa2401106@gmail.com>
2024-08-12 00:25:51 +0900

    add:orderDataの取得,Jotai管理ライブラリの追加

---
+++ src/firebase/FirebaseUtils.ts
@@ -0,0 +1,42 @@
+import { collection, getDocs } from "firebase/firestore";
+import { atom } from 'jotai'
+import { db } from "./firebase";
+
+// firebaseのエラーを判定する関数
+//型ガードを使用する
+function isFirebaseError(
+    err: unknown
+  ): err is { code: string; message: string } {
+    return typeof err === "object" && err !== null && "code" in err;
+  }
+
+
+//データを取得する関数
+
+export const fetchOrder = async () => {
+    try {
+        const querySnapshot = await getDocs(collection(db, "order"));
+        console.log(querySnapshot);
+
+        const OrderData = querySnapshot.docs.map((doc) => {
+            return {
+                ...doc.data(),
+                id: doc.id,
+              };
+            });
+
+
+} catch (err) {        // エラー処理
+    if (isFirebaseError(err)) {
+      console.error("Firestore Error:", err);
+
+    } else {
+      console.error("一般的なエラー", err);
+    }
+  } finally {
+    console.log("finally");
+
+  }
+};
+
+export const orderAtom = atom(async () => await fetchOrder());
