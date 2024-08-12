d016de33588361fafcc7aa07ee5526b232372bc2
tanaka0005 <tanaka00005.com>
2024-08-12 11:54:29 +0900

    add:注文内容確認・変更画面の左側の機能作成

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import OrderChange from './pages/OrderChange.tsx';
 
 const root = document.getElementById('root');
 if (root == null) throw new Error('Root element not found');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <OrderChange />
   </React.StrictMode>,
 );
