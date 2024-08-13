fdab5547490c61bb56eee9cc39d8e77584d3c9ed
tanaka0005 <tanaka00005.com>
2024-08-13 21:46:59 +0900

    update:イラストの配列作成

--- src/components/OrderMenuLeft.tsx
+++ src/components/OrderMenuLeft.tsx
@@ -3,25 +3,26 @@ import OrderMenueContena from '../components/OrderMenueContena';
 import OrderMenueCustomize from '../components/OrderMenueCustomize';
 import OrderMenueName from '../components/OrderMenueName';
 import { Box, Grid, Stack } from '@mui/material';
-
-
+import Yakitori from '/yakitori.png';
+import YakitoriSolt from '/yakitori_solt.png';
+import Negima from '/negima.png';
   
 
 function OrderMenuLeft() {
     const ordernames = [
-        { name: '塩', price: 300 },
-        { name: 'たれ', price: 300 },
-        { name: 'ねぎま', price: 300 },
-        { name: 'もも', price: 300 },
-        { name: 'コーラ', price: 100 },
-        { name: 'ファンタグレープ', price: 100 },
+        { name: '塩', price: 300 , img:Yakitori},
+        { name: 'たれ', price: 300, img:YakitoriSolt},
+        { name: 'ねぎま', price: 300 ,img:Negima},
+        { name: 'もも', price: 300 ,img:YakitoriSolt} ,
+        { name: 'コーラ', price: 100 , img:Yakitori},
+        { name: 'ファンタグレープ', price: 100 ,img:Yakitori},
       ];
   return (
     <div>
        <Box>
           <Box>
             {ordernames.map((order) => (
-              <OrderMenueContena ordername={order.name} orderprice={order.price} />
+              <OrderMenueContena ordername={order.name} orderprice={order.price} orderimg={order.img}/>
             ))}
           </Box>
         </Box>
