5c7e63ed47246ef34c1a097223ca594ff803b47b
watagassa <k23095kk@aitech.ac.jp>
2024-08-14 01:44:31 +0900

    add:注文番号

--- src/components/OrderNumber.tsx
+++ src/components/OrderNumber.tsx
@@ -3,16 +3,30 @@ import { grey } from '@mui/material/colors';
 
 const OrderNumber = () => (
   <div>
-    <Grid container 
-    // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+    <Grid
+      container
+      // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      sx={{
+        display:"flex",
+        flexFlow:'column',
+        textAlign:'center',
+        width:100,
+        position:'fixed',
+        top:80,
+        right:30,
+      }}
     >
-      <Card sx={{
-        backgroundColor:grey[300],
-        fontSize: 40,
-        minWidth: 160,
-        
-      }}>185</Card>
-      
+    注文番号
+      <Card
+        sx={{
+          backgroundColor: grey[300],
+          fontSize: 40,
+          textAlign: 'center',
+
+        }}
+      >
+        185
+      </Card>
     </Grid>
   </div>
 );
--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,7 +1,11 @@
+import OrderNumber from '../components/OrderNumber';
+
 const Order = () => (
   <div>
     <h1>Home</h1>
     <p>Homeページです</p>
+    {/* 注文番号のtest用 あとで消す */}
+    <OrderNumber></OrderNumber>
   </div>
 );
 
