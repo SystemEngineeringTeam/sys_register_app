a83378414cdf091fcc59eaf22aa44931e612738a
tanaka0005 <tanaka00005.com>
2024-08-13 19:46:59 +0900

    update:確認しやすいようにした

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import OrderChange from './pages/OrderChange.tsx';
+import OrderMenuRight from './components/OrderMenuRight.tsx';
 
 const root = document.getElementById('root');
 if (root == null) throw new Error('Root element not found');
