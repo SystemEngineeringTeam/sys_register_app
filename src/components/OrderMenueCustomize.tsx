9ea8cff5fec27c0b1d9781afb49f031af9754aad
tanaka0005 <tanaka00005.com>
2024-08-13 19:45:07 +0900

    update:カスタマイズの価格を配列にして表示するようにした

--- src/components/OrderMenueCustomize.tsx
+++ src/components/OrderMenueCustomize.tsx
@@ -1,17 +1,18 @@
 import { Box, Stack } from '@mui/material';
 import React from 'react';
 
-interface OrderMenueContenaProps {
+interface OrderMenueCustomizeProps {
   custom: string;
+  customprice:number;
 }
 
-const OrderMenueCustomize = ({ custom }: OrderMenueContenaProps) => {
+const OrderMenueCustomize = ({ custom, customprice}: OrderMenueCustomizeProps) => {
   return (
     <div>
       <Stack direction="row" sx={{ borderBottom: '2px solid #2b2b2b', flex: 3 }}>
         
           <Box sx={{flex:1}}>{custom}</Box>
-          <Box sx={{flex:1,textAlign:'right'}}>+20</Box>
+          <Box sx={{flex:1,textAlign:'right'}}>+{customprice}</Box>
         
       </Stack>
     </div>
