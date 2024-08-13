24175baea48be2df44c4852b25593ae9504fd9ce
tanaka0005 <tanaka00005.com>
2024-08-13 19:46:23 +0900

    add:お支払いへボタンの追加

---
+++ src/components/OrderButton.tsx
@@ -0,0 +1,27 @@
+import { Box, Grid } from '@mui/material'
+import React from 'react'
+
+function OrderButton() {
+  return (
+    <div>
+      <Grid item sx={{ 
+        background:'#F68B1F',
+        ml: '25%',
+        padding: '10px 20px', // 必要に応じてパディングを調整
+        position:'fixed',
+        bottom:'30px',
+        color:'#FFFFFF',
+       fontSize:{xs:"none",sm:"1.5rem",md:"2rem"},
+       display: {
+        xs: 'none', // 600px未満の画面幅では非表示
+        sm: 'none', // 600px〜900pxの画面幅では非表示
+        md: 'block', // 900px以上の画面幅では表示
+      },
+        }}>
+        お支払いへ
+      </Grid>
+    </div>
+  )
+}
+
+export default OrderButton
