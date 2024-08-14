8f65567ba1d0250a3e2d6c6c035102bdba71e358
tanaka0005 <tanaka00005.com>
2024-08-14 15:25:35 +0900

    update:レスポンシブ流デザインに対応するようにした

--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -6,12 +6,12 @@ function OrderButton() {
     <div>
       <Grid item sx={{ 
         background:'#F68B1F',
-        ml: '25%',
+        ml: '28%',
         padding: '10px 20px', // 必要に応じてパディングを調整
         position:'fixed',
         bottom:'30px',
         color:'#FFFFFF',
-       fontSize:{xs:"none",sm:"1.5rem",md:"2rem"},
+       fontSize:{xs:"none",sm:"none",md:"1.5rem"},
        display: {
         xs: 'none', // 600px未満の画面幅では非表示
         sm: 'none', // 600px〜900pxの画面幅では非表示
