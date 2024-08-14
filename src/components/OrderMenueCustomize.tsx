20ffe0611f918b5db72bb65ac8fc3af935ac722c
tanaka0005 <tanaka00005.com>
2024-08-14 17:35:25 +0900

    update:見やすいように動かした

--- src/components/OrderMenueCustomize.tsx
+++ src/components/OrderMenueCustomize.tsx
@@ -11,7 +11,7 @@ const OrderMenueCustomize = ({ custom, customprice}: OrderMenueCustomizeProps) =
     <div>
       <Stack direction="row" sx={{ borderBottom: '1px solid #2b2b2b'}}>
         
-          <Box sx={{pl:'30px'}}>{custom}</Box>
+          <Box sx={{ml:'10px'}}>{custom}</Box>
           <Box sx={{ml:'auto'}}>+{customprice}</Box>
         
       </Stack>
