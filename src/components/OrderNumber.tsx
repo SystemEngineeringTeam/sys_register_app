3109c9d84ed9026b50c63b136b64dba7bb0c8c4c
watagassa <k23095kk@aitech.ac.jp>
2024-08-14 18:35:32 +0900

    update:xs,sm,mdに対応

--- src/components/OrderNumber.tsx
+++ src/components/OrderNumber.tsx
@@ -5,22 +5,22 @@ const OrderNumber = () => (
   <div>
     <Grid
       container
-      // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
       sx={{
         display:"flex",
         flexFlow:'column',
         textAlign:'center',
-        width:100,
+        fontSize: { xs: "0.8rem", sm: "1rem", md: "1.5rem" },
+        width:{ xs: "6rem", sm: "8rem", md: "10rem" },
         position:'fixed',
-        top:80,
-        right:30,
+        top:{ xs: "4rem", sm: "4.5rem", md: "4.5rem" },
+        right:10,
       }}
     >
     注文番号
       <Card
         sx={{
           backgroundColor: grey[300],
-          fontSize: 40,
+          fontSize: { xs: "3rem", sm: "4rem", md: "5rem" },
           textAlign: 'center',
 
         }}
