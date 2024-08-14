0f2b5a1ec0575a2b4f6dbd0248c19441ca011ec9
tanaka0005 <tanaka00005.com>
2024-08-14 14:51:36 +0900

    update:線を薄くして見やすくなるように

--- src/components/OrderMenueCustomize.tsx
+++ src/components/OrderMenueCustomize.tsx
@@ -9,7 +9,7 @@ interface OrderMenueCustomizeProps {
 const OrderMenueCustomize = ({ custom, customprice}: OrderMenueCustomizeProps) => {
   return (
     <div>
-      <Stack direction="row" sx={{ borderBottom: '2px solid #2b2b2b', flex: 3 }}>
+      <Stack direction="row" sx={{ borderBottom: '1px solid #2b2b2b', flex: 3 }}>
         
           <Box sx={{flex:1}}>{custom}</Box>
           <Box sx={{flex:1,textAlign:'right'}}>+{customprice}</Box>
