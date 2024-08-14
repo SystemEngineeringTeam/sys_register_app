aab4430c2e209ff309aef01a3ec8555a25b9999b
tanaka0005 <tanaka00005.com>
2024-08-14 16:48:16 +0900

    update:画面の比率を調節した

--- src/components/OrderMenueName.tsx
+++ src/components/OrderMenueName.tsx
@@ -16,10 +16,10 @@ const OrderMenueName = ({ordername,orderprice}:OrderMenueNameProps) => {
     <div style={{display:"flex"}}>
       
         {/* {menuname} */}
-        <Box sx={{fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem",fontSize:'30px'}}}><CreateIcon /></Box>
-        <Box flex={.4} sx={{wordBreak:"break-word", fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}} >{ordername}</Box>
-        <Box sx={{ml:{xs:"30px",sm:"50px",md:"200px"}, fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}}>x2</Box>
-        <Box sx={{ml: 'auto',fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}}>{orderprice}</Box>
+        <Box sx={{fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}}><CreateIcon /></Box>
+        <Box flex={3} sx={{ml:'30px',wordBreak:"break-word", fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}}>{ordername}</Box>
+        <Box flex={1.5} sx={{ml:{xs:"30px",sm:"50px",md:"520px"}, fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"},}}>x2</Box>
+        <Box  sx={{ml: 'auto',fontSize:{xs:"1.2rem",sm:"1.5rem",md:"2rem"}}}>{orderprice}</Box>
       
     </div>
   )
