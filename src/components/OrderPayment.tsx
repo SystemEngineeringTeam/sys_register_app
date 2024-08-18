d926215b0b7e923a05ad88c627b2ead7c4decff3
watagassa <k23095kk@aitech.ac.jp>
2024-08-19 00:31:50 +0900

    update:MoneyPaidをCollectedMoneyPaidにまとめた

--- src/components/OrderPayment.tsx
+++ src/components/OrderPayment.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import OrderNumber from './OrderNumber';
-import MoneyPaid from './MoneyPaid';
+import CollectedMoneyPaid from './CollectedMoneyPaid';
+
 
 const OrderPayment = () => {
   return (
     <div>
-      <OrderNumber />
-      <MoneyPaid image="/money_1.svg" />
-      <MoneyPaid image="/money_5.svg" />
-      <MoneyPaid image="/money_10.svg" />
-      <MoneyPaid image="/money_50.svg" />
-      <MoneyPaid image="/money_100.svg" />
-      <MoneyPaid image="/money_500.svg" />
-      <MoneyPaid image="/money_1000.svg" />
-      <MoneyPaid image="/money_5000.svg" />
-      <MoneyPaid image="/money_10000.svg" />
+      <OrderNumber/>
+      <CollectedMoneyPaid />
     </div>
   );
 };
