218bb2b36a9a37dc7a1fcbfbf22b233464fd889f
tanaka0005 <tanaka00005.com>
2024-08-18 16:13:15 +0900

    update:BoxタグをButtonタグへ変更

--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -1,24 +1,21 @@
-import { Box, Grid } from '@mui/material'
+import { Box, Button, Grid } from '@mui/material'
 import React from 'react'
 
 function OrderButton() {
   return (
     <div>
-      <Grid item sx={{ 
+      <Button variant='contained' disableElevation size='large' onClick=
+      {() => {
+        console.log('お支払いへ')
+    
+      }}
+       sx={{ 
         background:'#F68B1F',
-        padding: '10px 20px', // 必要に応じてパディングを調整
-        position:'fixed',
-        bottom:'20px',
-        color:'#FFFFFF',
-       fontSize:{xs:"none",sm:"none",md:"1.5rem"},
-       display: {
-        xs: 'none', // 600px未満の画面幅では非表示
-        sm: 'none', // 600px〜900pxの画面幅では非表示
-        md: 'block', // 900px以上の画面幅では表示
-      },
+        py: '20px', // 必要に応じてパディングを調整
+        width:'100%',
         }}>
         お支払いへ
-      </Grid>
+      </Button>
     </div>
   )
 }
