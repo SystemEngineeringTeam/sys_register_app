749f4857690abc0aa896e8bf930dc7320a0113c5
tanaka0005 <tanaka00005.com>
2024-08-18 16:09:26 +0900

    update:全体を固定して左部分だけスクロールできるようにした

--- src/pages/OrderChange.tsx
+++ src/pages/OrderChange.tsx
@@ -7,17 +7,19 @@ import OrderButton from '../components/OrderButton';
 const OrderChange = () => {
   return (
     <div>
-      <Stack direction="row" >
+      <Box sx={{display:'flex', height: '100vh'}}>
         {/* 左側メニューリスト */}
-        <Box >
+        <Box sx={{flex:4,overflowY: 'auto'}}>
           <OrderMenuLeft />
         </Box>
 
         {/* 右側注文情報 */}
-        <Box sx={{display:'flex',justifyContent:'center',textAlign:'center'}}>
-          <OrderMenuRight />
-        </Box>
-      </Stack>
+
+            <Box sx={{flex:1}}>
+                 <OrderMenuRight />
+            </Box>
+
+      </Box>
     </div>
   );
 };
