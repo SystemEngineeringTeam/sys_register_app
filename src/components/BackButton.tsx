3840a0d3aaaada44436d3b87797e24d406f7489f
watagassa <k23095kk@aitech.ac.jp>
2024-08-20 01:17:18 +0900

    add:戻るボタン

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+import { grey } from "@mui/material/colors"
+
+const BackButton = () => {
+  return (
+    <Button sx={{backgroundColor:grey[500],color:'#ffffff'}}>戻る</Button>
+  )
+}
+
+export default BackButton
