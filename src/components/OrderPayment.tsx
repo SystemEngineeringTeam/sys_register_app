8d3c91417282d1379ead95148ca63de6f141aeb5
watagassa <k23095kk@aitech.ac.jp>
2024-08-20 01:05:30 +0900

    update:金額表示

--- src/components/OrderPayment.tsx
+++ src/components/OrderPayment.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
 import OrderNumber from './OrderNumber';
 import CollectedMoneyPaid from './CollectedMoneyPaid';
-
+import { Box } from '@mui/material';
+import WritePaidSumMoney from './WritePaidSumMoney';
+import WriteNotEnoughMoney from './WriteNotEnoughMoney';
+import { BorderAll } from '@mui/icons-material';
 
 const OrderPayment = () => {
   return (
-    <div>
-      <OrderNumber/>
+    <Box sx={{display:'flex'}}>
+      {/* OrderNumberはマージ終わったらsxを記入する */}
+      {/* 注文番号 */}
+      <OrderNumber />
+      {/* 通貨 */}
       <CollectedMoneyPaid />
-    </div>
+      <Box sx={{marginTop:'10rem',marginLeft:'2rem'}}>
+        {/* お支払いと合計金額 */}
+        <WritePaidSumMoney />
+
+        <Box sx={{ border: 1 }}></Box>
+        {/* おつり */}
+        <WriteNotEnoughMoney />
+      </Box>
+    </Box>
   );
 };
 
