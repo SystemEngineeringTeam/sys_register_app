b25e0b3b1c1eb1adee1c0dd8e3570ee8b6b218da
watagassa <k23095kk@aitech.ac.jp>
2024-08-20 11:54:20 +0900

    add:OKButton作成

---
+++ src/components/OkButton.tsx
@@ -0,0 +1,25 @@
+import { Button, createTheme, PaletteColor, PaletteColorOptions, ThemeProvider } from '@mui/material';
+import { grey, orange } from '@mui/material/colors';
+import { theme } from '../themes/theme';
+
+
+
+const OkButton = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button
+        variant="contained"
+        color="ok"
+        sx={{
+          fontSize:{ xs: '1.5rem', sm: '2rem', md: '2.5rem' },
+          width: { xs: '4rem', sm: '6rem', md: '8rem' },
+          height: { xs: '2.5rem', sm: '3rem', md: '3.5rem' },
+        }}
+      >
+        OK
+      </Button>
+    </ThemeProvider>
+  );
+};
+
+export default OkButton;
