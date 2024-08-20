d94a9da82793715ed96aeffe007622eaca286dcb
watagassa <k23095kk@aitech.ac.jp>
2024-08-20 11:54:47 +0900

    add:OKButton追加

--- src/components/OrderPayment.tsx
+++ src/components/OrderPayment.tsx
@@ -3,26 +3,31 @@ import CollectedMoneyPaid from './CollectedMoneyPaid';
 import { Box } from '@mui/material';
 import WritePaidSumMoney from './WritePaidSumMoney';
 import WriteNotEnoughMoney from './WriteNotEnoughMoney';
-import { BorderAll } from '@mui/icons-material';
 import BackButton from './BackButton';
+import OkButton from './OkButton';
 
 const OrderPayment = () => {
   return (
-    <Box sx={{display:'flex'}}>
+    <Box sx={{ display: 'flex' }}>
       {/* OrderNumberはマージ終わったらsxを記入する */}
       {/* 注文番号 */}
       <OrderNumber />
       {/* 通貨 */}
       <CollectedMoneyPaid />
-      <Box sx={{marginTop:'10rem',marginLeft:'2rem'}}>
+      <Box sx={{ marginTop: '10rem', marginLeft: '2rem' }}>
         {/* お支払いと合計金額 */}
         <WritePaidSumMoney />
 
         <Box sx={{ border: 1 }}></Box>
         {/* おつり */}
         <WriteNotEnoughMoney />
-        <Box>
-          <BackButton />
+        <Box sx={{display:'flex'}}>
+          <Box>
+            <BackButton />
+          </Box>
+          <Box>
+            <OkButton />
+          </Box>
         </Box>
       </Box>
     </Box>
