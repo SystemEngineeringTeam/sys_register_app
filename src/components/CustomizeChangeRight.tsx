66f243f9bdbdb2ff4088fe1baba50fac098a0c7d
tanaka0005 <tanaka00005.com>
2024-08-22 12:56:05 +0900

    update:レスポンシブルデザインに対応するようにした

--- src/components/CustomizeChangeRight.tsx
+++ src/components/CustomizeChangeRight.tsx
@@ -10,7 +10,7 @@ function CustomizeChangeRight() {
       <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
         <Box sx={{ fontSize: '30px', mt: '50px' }}>注文番号</Box>
 
-        <Box sx={{mt:'520px'}}>
+        <Box sx={{mt:'100%'}}>
         <Box > <OrderDelete /> </Box>
         <Box sx={{mt:'30px'}}><OkeyButton /></Box> 
         </Box>
