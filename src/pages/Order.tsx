7e98eff2cff3815812715c3011ded41f5afdbca6
tanaka0005 <tanaka00005.com>
2024-09-06 19:21:13 +0900

    add:ordersの中身を加工した値を入れた

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -3,17 +3,42 @@ import NumberButton from '../components/NumberButton';
 import { Box, Grid } from '@mui/material';
 import NumberButtonBox from '../components/NumberButtonBox';
 import OrderWaitPeople from '../components/OrderWaitPeople';
+import { orderCollectionAtom } from '../firebase/FirebaseUtils';
+import { useAtom } from 'jotai';
+import { processOrderCollection } from '../utils/processOrderCollection';
 
 const Order = () => {
-  const orders = [
-    1, 2, 3, 4, 4, 5, 4, 231, 3245, 324, 332, 344, 223, 421, 324, 321, 123, 242, 234, 231, 324, 23, 4, 234, 443, 244,
-    232,
-  ];
-  return (
-    <div>
-      <NumberButtonBox orders={orders} />
-      <OrderWaitPeople orders={orders} />
-    </div>
-  );
+  // const orders = [
+  //   1, 2, 3, 4, 4, 5, 4, 231, 3245, 324, 332, 344, 223, 421, 324, 321, 123, 242, 234, 231, 324, 23, 4, 234, 443, 244,
+  //   232,
+  // ];
+
+  const [orderCollectionData, setOrderCollectionData] = useAtom(orderCollectionAtom);
+
+
+    switch (orderCollectionData.state) {
+      case 'loading': 
+        return <p>Loading...</p>;
+
+      case 'hasError':
+        return <p>Error</p>;
+
+      case 'hasData':
+
+        const order = processOrderCollection(orderCollectionData.data || []);
+        console.log("🚀 ~ Order ~ order:", order)
+
+        const orders = order.map((order) => Number(order.id))
+        //const orders = [Number(order)];
+
+        console.log("🚀 ~ Order ~ orders:", orders);
+        return (
+          <div>
+            <NumberButtonBox orders={orders} />
+            <OrderWaitPeople orders={orders} />
+          </div>
+        );
+      
+  }
 };
 export default Order;
