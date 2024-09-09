3c4df515f319d03b02219b717ebabe235504e3e6
tanaka0005 <tanaka00005.com>
2024-09-09 16:43:03 +0900

    update:戻り値の型定義とaccountingがtrueの時だけ返すようにした

--- src/utils/processOrderCollection.ts
+++ src/utils/processOrderCollection.ts
@@ -1,10 +1,15 @@
 // utils/processOrderCollection.ts
 import { orderCollection } from '../types/index';
 
-export const processOrderCollection = (orderCollections: orderCollection[]) => {
-  return orderCollections.map(orderCollection => {
-    return {
-      id: orderCollection.id,
-    }
-})
+export const processOrderCollection = (orderCollections: orderCollection[]):{ id: string | null }[] => {
+  return orderCollections
+  .filter(orderCollection => orderCollection.accounting === true)
+  .map(orderCollections => {
+    return{
+        id: orderCollections.id}
+    } 
+    
+)
+       
+
 }
