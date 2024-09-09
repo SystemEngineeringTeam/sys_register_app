c68c7558de8dd895b5c275b5eaad837c9b16c104
tanaka0005 <tanaka00005.com>
2024-09-09 16:51:29 +0900

    update:falseとtrue逆だった

--- src/utils/processOrderCollection.ts
+++ src/utils/processOrderCollection.ts
@@ -3,7 +3,7 @@ import { orderCollection } from '../types/index';
 
 export const processOrderCollection = (orderCollections: orderCollection[]):{ id: string | null }[] => {
   return orderCollections
-  .filter(orderCollection => orderCollection.accounting === true)
+  .filter(orderCollection => orderCollection.accounting != true)
   .map(orderCollections => {
     return{
         id: orderCollections.id}
