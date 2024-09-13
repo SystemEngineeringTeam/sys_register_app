d6d21aed94eaf2005def1b213e67e5b8d73f1cb4
tanaka0005 <tanaka00005.com>
2024-09-13 22:01:09 +0900

    update:加工してデータベース上のデータ持ってきた

--- src/components/OrderMenuLeft.tsx
+++ src/components/OrderMenuLeft.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
-import OrderMenueContena from '../components/OrderMenueContena';
-import OrderMenueCustomize from '../components/OrderMenueCustomize';
-import OrderMenueName from '../components/OrderMenueName';
-import { Box, Grid, Stack } from '@mui/material';
-import Yakitori from '/yakitori.png';
-import YakitoriSolt from '/yakitori_solt.png';
-import Negima from '/negima.png';
+import NumberButton from '../components/NumberButton';
+import { Box, Grid } from '@mui/material';
+import NumberButtonBox from '../components/NumberButtonBox';
+import OrderWaitPeople from '../components/OrderWaitPeople';
+import { orderCollectionAtom } from '../firebase/FirebaseUtils';
+import { useAtom } from 'jotai';
+import { processOrderCollection } from '../utils/processOrderCollection';
+import { processOrderChange } from '../utils/processOrderChange';
+import OrderMenueContena from './OrderMenueContena';
+import Yakitori  from '/yakitori_solt.png'
 
-function OrderMenuLeft() {
-  const ordernames = [
-    { name: '塩', price: 300, img: Yakitori },
-    { name: 'たれ', price: 300, img: YakitoriSolt },
-    { name: 'ねぎま', price: 300, img: Negima },
-    { name: 'もも', price: 300, img: YakitoriSolt },
-    { name: 'コーラ', price: 100, img: Yakitori },
-    { name: 'ファンタグレープ', price: 100, img: Yakitori },
-  ];
-  return (
-    <div>
-      <Box>
-        <Box>
-          {ordernames.map((order) => (
-            <OrderMenueContena ordername={order.name} orderprice={order.price} orderimg={order.img} />
-          ))}
-        </Box>
-      </Box>
-    </div>
-  );
-}
+const OrderMenuLeft = () => {
+  // const orders = [
+  //   1, 2, 3, 4, 4, 5, 4, 231, 3245, 324, 332, 344, 223, 421, 324, 321, 123, 242, 234, 231, 324, 23, 4, 234, 443, 244,
+  //   232,
+  // ];
 
+  const [orderCollectionData, setOrderCollectionData] = useAtom(orderCollectionAtom);
+
+  switch (orderCollectionData.state) {
+    case 'loading':
+      return <p>Loading...</p>;
+
+    case 'hasError':
+      return <p>Error</p>;
+
+    case 'hasData':
+      const processedOptions = processOrderChange(
+        (orderCollectionData.data || [])
+        .flatMap((order) => order.order.flatMap((o) => o.item)),
+      );
+      console.log('🚀 ~ Order ~ order:', processedOptions);
+
+
+      return (
+        <div>
+          <Box>
+            <Box>
+              {processedOptions.map((order) => (
+                <OrderMenueContena ordername={order.name || ''} orderprice={order.price || 0} orderimg={order.name || ''} />
+              ))}
+            </Box>
+          </Box>
+        </div>
+      );
+  }
+};
 export default OrderMenuLeft;
