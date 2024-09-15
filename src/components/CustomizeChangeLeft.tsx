4445d97b2d3b5f010d31374a67a5e49747b88e76
tanaka0005 <tanaka00005.com>
2024-09-15 15:02:10 +0900

    add:データベースの値が出るようにした

--- src/components/CustomizeChangeLeft.tsx
+++ src/components/CustomizeChangeLeft.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import CustomizeMenu from './CustomizeMenu';
 import { Box } from '@mui/material';
 import CustmizeGraf from './CustmizeGraf';
+import { useAtom } from 'jotai';
+import { orderCollectionAtom } from '../firebase/FirebaseUtils';
+import { processCustomizeChange } from '../utils/processCustomizeChange';
+import { processCustomize } from '../utils/processCustomize';
 
 function CustomizeChangeLeft() {
-  const customizechanges = ['カスタマイズ1', 'カスタマイズ2', 'カスタマイズ3'];
+  //const customizechanges = ['カスタマイズ1', 'カスタマイズ2', 'カスタマイズ3'];\
+
+  const [orderCollectionData, setOrderCollectionData] = useAtom(orderCollectionAtom);
+
+  switch (orderCollectionData.state) {
+    case 'loading':
+      return <p>Loading...</p>;
+
+    case 'hasError':
+      return <p>Error</p>;
+
+    case 'hasData':
+    const processCustmize = processCustomize((orderCollectionData.data || [])
+    .flatMap((order) => 
+      order.order.flatMap((o) => 
+        o.options 
+      ))
+  )
+
+  console.log("🚀 ~ CustomizeChangeLeft ~ processCustmize:", processCustmize)
 
   return (
+    
     <div>
       <Box sx={{ ml: '50px' }}>
         <Box>
@@ -14,13 +38,13 @@ function CustomizeChangeLeft() {
         </Box>
         <Box sx={{ fontSize: '30px' }}>カスタマイズ</Box>
         <Box>
-          {customizechanges.map((order) => (
-            <CustmizeGraf customize={order} />
+          {processCustmize.map((order) => (
+            <CustmizeGraf customize={order.name || ''} />
           ))}
         </Box>
       </Box>
     </div>
   );
 }
-
+}
 export default CustomizeChangeLeft;
