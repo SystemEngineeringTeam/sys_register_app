726e0a34a101e9c5618f88417bf763a110d0c6ed
tanaka0005 <tanaka00005.com>
2024-09-17 14:48:54 +0900

    update:データベースを加工する処理の消去

--- src/components/OrderMenuLeft.tsx
+++ src/components/OrderMenuLeft.tsx
@@ -10,40 +10,43 @@ import { processOrderChange } from '../utils/processOrderChange';
 import OrderMenueContena from './OrderMenueContena';
 import Yakitori  from '/yakitori_solt.png'
 
-const OrderMenuLeft = () => {
+interface OrderMenueLeftProps {
+  orders:any;
+  processedoptions: {
+    name: string | null;
+    price: number | null;
+  }[];
+
+}
+
+const OrderMenuLeft = ({orders, processedoptions}: OrderMenueLeftProps) => {
   // const orders = [
   //   1, 2, 3, 4, 4, 5, 4, 231, 3245, 324, 332, 344, 223, 421, 324, 321, 123, 242, 234, 231, 324, 23, 4, 234, 443, 244,
   //   232,
   // ];
 
-  const [orderCollectionData, setOrderCollectionData] = useAtom(orderCollectionAtom);
+  
+      
 
-  switch (orderCollectionData.state) {
-    case 'loading':
-      return <p>Loading...</p>;
-
-    case 'hasError':
-      return <p>Error</p>;
-
-    case 'hasData':
-      const processedOptions = processOrderChange(
-        (orderCollectionData.data || [])
-        .flatMap((order) => order.order.flatMap((o) => o.item)),
-      );
-      console.log('🚀 ~ Order ~ order:', processedOptions);
-
+      console.log("processedOptions:"+processedoptions);
 
       return (
+        
         <div>
           <Box>
             <Box>
-              {processedOptions.map((order) => (
-                <OrderMenueContena ordername={order.name || ''} orderprice={order.price || 0} orderimg={order.name || ''} />
-              ))}
+            {processedoptions.map((order) => (
+            <OrderMenueContena
+              ordername={order.name || ''}
+              orderprice={order.price || 0}
+              orderimg={order.name || ''}
+            />
+            ))}
+ 
             </Box>
           </Box>
         </div>
       );
   }
-};
+
 export default OrderMenuLeft;
