84d7417d2f2e513a749791d6b52b4e9e50c6a43e
tanaka0005 <tanaka00005.com>
2024-09-20 11:57:35 +0900

    update:データベースを取ってくるやり方を変えた

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import NumberButton from '../components/NumberButton';
 import { Box, Grid } from '@mui/material';
 import NumberButtonBox from '../components/NumberButtonBox';
@@ -8,43 +8,38 @@ import { useAtom } from 'jotai';
 import { processOrderCollection } from '../utils/processOrderCollection';
 import { processNumber, processOrderChange } from '../utils/processOrderChange';
 import { processCustomizeChange } from '../utils/processCustomizeChange';
+import { useOrderCollection } from '../firebase/useOrderCollection';
+import { useMoney } from '../firebase/useMoney';
 
-const Order = () => {
   // const orders = [
   //   1, 2, 3, 4, 4, 5, 4, 231, 3245, 324, 332, 344, 223, 421, 324, 321, 123, 242, 234, 231, 324, 23, 4, 234, 443, 244,
   //   232,
   // ];
 
-  const [orderCollectionData, setOrderCollectionData] = useAtom(orderCollectionAtom);
-
-  switch (orderCollectionData.state) {
-    case 'loading':
-      return <p>Loading...</p>;
-
-    case 'hasError':
-      return <p>Error</p>;
-
-    case 'hasData':
-      const order = processOrderCollection(orderCollectionData.data || []);
+  export default function Order(): ReactElement {
+    const { data } = useOrderCollection();
+    const { money} = useMoney();
+  
+      const order = processOrderCollection(data || []);
       console.log('🚀 ~ Order ~ order:', order);
 
       const orders = order.map((order) => Number(order.id));
       //const orders = [Number(order)];
 
       const menu = processOrderChange(
-        (orderCollectionData.data || []).flatMap((order) => order.order.flatMap((o) => o.item)),
+        (data || []).flatMap((order) => order.order.flatMap((o) => o.item)),
       );
 
-      const menuqty = processNumber((orderCollectionData.data || []).flatMap((order) => order.order));
+      const menuqty = processNumber((data || []).flatMap((order) => order.order));
 
       const processCustmize = processCustomizeChange(
-        (orderCollectionData.data || []).flatMap((order) => order.order.flatMap((o) => o.options)),
+        (data || []).flatMap((order) => order.order.flatMap((o) => o.options)),
       );
-      console.log("🚀 ~ Order ~ processCustmize:", processCustmize)
 
       console.log('🚀 ~ Order ~ orders:', orders);
       console.log('🚀 ~ Order ~ menu:', menu);
       console.log('🚀 ~ Order ~ menuqty:', menuqty);
+      console.log("🚀 ~ Order ~ processCustmize:", processCustmize);
 
       return (
         <div>
@@ -53,5 +48,5 @@ const Order = () => {
         </div>
       );
   }
-};
-export default Order;
+
+
