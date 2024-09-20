59cb79af3ce7c341b9109327d84f343e3507cd8f
tanaka0005 <tanaka00005.com>
2024-09-21 06:20:14 +0900

    update:usestateの導入

--- src/utils/processOrderCollection.ts
+++ src/utils/processOrderCollection.ts
@@ -1,11 +1,18 @@
 // utils/processOrderCollection.ts
 import { orderCollection } from '../types/index';
+import { useState } from 'react';
+
 
 export const processOrderCollection = (orderCollections: orderCollection[]):{ id: string | null }[] => {
 
   const aryId: { id: string | null}[] = [];
   const aryWaitPeople: {id: string | null}[] = [];
   const aryCallPeople:{ id:string | null}[] = [];
+  const [id,setId] = useState<{id: string | null}[]>([]);
+  const [waitPeople,setWaitPeople] = useState<{id: string | null}[]>([]);
+  const [callPeople,setCallPeople] = useState<{id: string | null}[]>([]);
+
+const ordernumber = ():{ id: string | null }[] => {
 
 return orderCollections.map((orderCollection,orderWait) => {
   console.log("id:"+orderCollection.id);
@@ -13,18 +20,36 @@ return orderCollections.map((orderCollection,orderWait) => {
   
   switch (orderCollection.accounting){
     case false:
+      
+      //setId(() => [...id,{ id: orderCollection.id}]);
+      // console.log("🚀 ~ returnorderCollections.map ~ setId:", setId)
+      // console.log("🚀 ~ returnorderCollections.map ~ setId:", id)
+      // console.log("🚀 ~ returnorderCollections.map ~ setId:", orderCollection.id)
+
       aryId.push({
         id:orderCollection.id
       })
+
+       setId((aryId))
+
+
+      console.log("🚀 ~ returnorderCollections.map ~ setId:", setId)
+      console.log("🚀 ~ returnorderCollections.map ~ setId:", id)
+
+
       return {
         id:orderCollection.id
       };
      
      case true:
       if(orderCollection.cooking = false){
+
+        setWaitPeople((waitpeople) => [...waitpeople,{id:orderCollection.id}])
+        console.log("🚀 ~ returnorderCollections.map ~ setWaitPeople:", setWaitPeople)
         aryWaitPeople.push({
           id:orderCollection.id
         })
+        setWaitPeople(aryWaitPeople)
       }
 
       console.log("aryWaitPeople:"+aryWaitPeople);
@@ -36,9 +61,14 @@ return orderCollections.map((orderCollection,orderWait) => {
       default: 
       if(orderCollection.cooking = false){
         if(orderCollection.offer = false){
+
+          setCallPeople((callpeople) => [...callpeople,{id: orderCollection.id}])
+          console.log("🚀 ~ returnorderCollections.map ~ setCallPeople:", setCallPeople)
+
           aryCallPeople.push({
             id:orderCollection.id
           })
+          setCallPeople(aryCallPeople)
         }
       }
 
@@ -47,8 +77,21 @@ return orderCollections.map((orderCollection,orderWait) => {
         id:orderCollection.id
       }
   }
+    
 })
+    
+
+
 }
 
+return ordernumber();
+};
+
+// const OrderNum = () => {
+//   const [number,setNumber] = useState([]);
+
+//   setNumber( se )
 
 
+//   setNumber
+// }
