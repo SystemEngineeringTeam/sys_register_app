7a673ac8ee1dfdca416e907cefc24d76cb1e45a4
watagassa <k23095kk@aitech.ac.jp>
2024-09-20 22:17:56 +0900

    update:レイアウト変更

--- src/components/OrderPayments/MoneyCount/MoneyCount.tsx
+++ src/components/OrderPayments/MoneyCount/MoneyCount.tsx
@@ -1,8 +1,11 @@
 // eslint-disable-next-line no-restricted-imports
 import OrderNumber from '../../OrderNumber';
 import { Box } from '@mui/material';
+// eslint-disable-next-line no-restricted-imports
 import WriteNotEnoughMoney from '../WriteNotEnoughMoney';
+// eslint-disable-next-line no-restricted-imports
 import BackButton from '../BackButton';
+// eslint-disable-next-line no-restricted-imports
 import OkButton from '../OkButton';
 import CollectedChenge from './CollectedChenge';
 
@@ -10,24 +13,28 @@ interface OrderPaymentProps {
   totalPayment: number;
   totalAmount: number;
 }
+// お釣り画面を表示する
 const MoneyCount = ({ totalPayment, totalAmount }: OrderPaymentProps) => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      {/* 注文番号 */}
-      <Box
-        sx={{
-          position: 'fixed',
-          top: { xs: '4rem', sm: '4.5rem', md: '4.5rem' },
-          right: 10,
-        }}
-      >
-        <OrderNumber />
+    <>
+      <Box sx={{ display: 'flex' }}>
+        {/* 注文番号 */}
+        <Box
+          sx={{
+            position: 'fixed',
+            top: { xs: '4rem', sm: '4.5rem', md: '4.5rem' },
+            right: 10,
+          }}
+        >
+          <OrderNumber />
+        </Box>
       </Box>
-
-      {/* 通貨と合計金額、おつり */}
-      <Box sx={{ display: { xs: 'block', sm: 'flex' } }}>
-        {/* 通貨を表示 */}
+      <Box sx={{ display: 'flex' }}>
         <CollectedChenge chenge={totalPayment - totalAmount} />
+      </Box>
+      {/* 通貨と合計金額、おつり */}
+      <Box sx={{ display: { xs: 'block', sm: 'flex' }, position: 'fixed', right: '2rem', bottom: '2rem' }}>
+        {/* 通貨を表示 */}
         <Box sx={{ marginTop: { sm: '25rem', md: '20rem' } }}>
           {/* おつり */}
           <WriteNotEnoughMoney totalAmount={totalAmount} totalPayment={totalPayment} />
@@ -43,7 +50,7 @@ const MoneyCount = ({ totalPayment, totalAmount }: OrderPaymentProps) => {
           </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
