6c915a877d56ceb37ba8b31bf398ca7bf93cd3ca
tanaka0005 <tanaka00005.com>
2024-09-20 21:11:11 +0900

    add:swichで配列に格納

--- src/utils/processOrderCollection.ts
+++ src/utils/processOrderCollection.ts
@@ -2,15 +2,53 @@
 import { orderCollection } from '../types/index';
 
 export const processOrderCollection = (orderCollections: orderCollection[]):{ id: string | null }[] => {
-  return orderCollections
-  .filter(orderCollection => orderCollection.accounting != true)
-  .map(orderCollections => {
-    return{
-        id: orderCollections.id
-    }
-    } 
-    
-)
-       
 
+  const aryId: { id: string | null}[] = [];
+  const aryWaitPeople: {id: string | null}[] = [];
+  const aryCallPeople:{ id:string | null}[] = [];
+
+return orderCollections.map((orderCollection,orderWait) => {
+  console.log("id:"+orderCollection.id);
+  console.log("boolean"+orderCollection.accounting)
+  
+  switch (orderCollection.accounting){
+    case false:
+      aryId.push({
+        id:orderCollection.id
+      })
+      return {
+        id:orderCollection.id
+      };
+     
+     case true:
+      if(orderCollection.cooking = false){
+        aryWaitPeople.push({
+          id:orderCollection.id
+        })
+      }
+
+      console.log("aryWaitPeople:"+aryWaitPeople);
+        return {
+          id:orderCollection.id
+        };
+
+
+      default: 
+      if(orderCollection.cooking = false){
+        if(orderCollection.offer = false){
+          aryCallPeople.push({
+            id:orderCollection.id
+          })
+        }
+      }
+
+      console.log("aryCallPeople:"+aryCallPeople);
+      return {
+        id:orderCollection.id
+      }
+  }
+})
 }
+
+
+
