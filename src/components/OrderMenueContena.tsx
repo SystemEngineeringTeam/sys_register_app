e64c4e4101d3eee9a1b66d5ccc174f777b382834
tanaka0005 <tanaka00005.com>
2024-09-25 15:18:21 +0900

    update:余白入れてみやすくした

--- src/components/OrderMenueContena.tsx
+++ src/components/OrderMenueContena.tsx
@@ -59,7 +59,7 @@ const OrderMenueContena = ({
         </Stack>
 
         <Stack sx={{ flex: 5 }}>
-          <Stack>
+          <Stack sx={{mr:'20px'}}>
             <OrderMenueName
               ordername={ordername}
               orderprice={orderprice}
@@ -67,7 +67,7 @@ const OrderMenueContena = ({
               customizename={customizename}
             />
           </Stack>
-          <Stack>
+          <Stack sx={{ml:'20px', mr:'20px'}}>
             <OrderMenueCustomize custom={customizename} customprice={customizeprice} />
           </Stack>
         </Stack>
