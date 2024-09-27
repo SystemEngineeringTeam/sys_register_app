69a0bd1ab6518b5796e03edd889736eb3f7fa169
tanaka0005 <tanaka00005.com>
2024-09-27 16:59:07 +0900

    update:ビューの変更

--- src/components/CustomizeChangeRight.tsx
+++ src/components/CustomizeChangeRight.tsx
@@ -3,24 +3,39 @@ import React from 'react';
 import OrderButton from './OrderButton';
 import OkeyButton from './OkeyButton';
 import OrderDelete from './OrderDelete';
+import OrderNumber from './OrderNumber';
 
-function CustomizeChangeRight() {
+interface CustomizeChangeRightProps {
+  id:string;
+}
+
+function  CustomizeChangeRight({id}:CustomizeChangeRightProps) {
+  
   return (
     <div>
       <Box sx={{ 
-        flexDirection: 'column',
-        position: 'fixed',
-        height: '100vh', 
+        position: 'fixed', // スクロールしても固定
+        height: '100vh',
         width: '20vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        
         }}>
-        <Box sx={{ fontSize: '30px' , mt:'20px'}}>注文番号</Box>
+        <Box sx={{ fontSize: '30px' }}>
+          {/* 注文番号表示 */}
+        <Box sx={{ mt : '50px' }}>
+          <OrderNumber id={id} />
+        </Box>
+        </Box>
 
-        <Box sx={{ mt:'500px' }}>
+        <Box sx={{ mt:'120%' }}>
           <Box>
             <OrderDelete />
           </Box>
-          <Box sx={{ mt:'50px'}}>
+          <Box sx={{ mt:'20%'}}>
             <OkeyButton />
           </Box>
         </Box>
