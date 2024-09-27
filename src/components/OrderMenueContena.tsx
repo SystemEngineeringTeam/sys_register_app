1ed31ec36c58534e943e9111946a0326106907de
tanaka0005 <tanaka00005.com>
2024-09-27 11:44:18 +0900

    update:名前の変更

--- src/components/OrderMenueContena.tsx
+++ src/components/OrderMenueContena.tsx
@@ -3,21 +3,21 @@ import OrderMenueCustomize from './OrderMenueCustomize';
 import OrderMenueName from './OrderMenueName';
 
 interface OrderMenueContenaProps {
-  ordername: string;
-  orderprice: number;
-  orderimg: string;
-  menuqty: number;
-  customizename: string;
-  customizeprice: number;
+  selectMenuName: string;
+  selectMenuPrice: number;
+  selectMenuImg: string;
+  selectMenuqty: number;
+  selectCustomizeName: string;
+  selectCustomizePrice: number;
 }
 
 const OrderMenueContena = ({
-  ordername,
-  orderprice,
-  orderimg,
-  menuqty,
-  customizename,
-  customizeprice,
+  selectMenuName,
+  selectMenuPrice,
+  selectMenuImg,
+  selectMenuqty,
+  selectCustomizeName,
+  selectCustomizePrice,
 }: OrderMenueContenaProps) => {
   const imageDisplaySize = { width: 200, height: 200 };
   // const custommenus = [
@@ -28,9 +28,9 @@ const OrderMenueContena = ({
   //   { menu: '青のり', price: 10 },
   // ];
 
-  console.log('menuqty:' + menuqty);
-  console.log('customizename:' + customizename);
-  console.log('customizeprice:' + customizeprice);
+  console.log('menuqty:' +  selectMenuName);
+  console.log('customizename:' + selectCustomizeName);
+  console.log('customizeprice:' + selectCustomizePrice);
 
   return (
     <div>
@@ -48,21 +48,21 @@ const OrderMenueContena = ({
         >
           {/* flexは幅の割合 */}
           <Box>
-            <img src={orderimg} className="yakitori" style={imageDisplaySize} />
+            <img src={selectMenuImg} className="yakitori" style={imageDisplaySize} />
           </Box>
         </Stack>
 
         <Stack sx={{ flex: 5 }}>
           <Stack sx={{mr:'20px'}}>
             <OrderMenueName
-              ordername={ordername}
-              orderprice={orderprice}
-              menuqty={menuqty}
-              customizename={customizename}
+              selectMenuName={selectMenuName}
+              selectMenuPrice={selectMenuPrice}
+              selectMenuqty={selectMenuqty}
+              selectCustomizeName={selectCustomizeName}
             />
           </Stack>
           <Stack sx={{ml:'20px', mr:'20px'}}>
-            <OrderMenueCustomize custom={customizename} customprice={customizeprice} />
+            <OrderMenueCustomize selectCustomizeName={selectCustomizeName} selectCustomizePrice={selectCustomizePrice} />
           </Stack>
         </Stack>
       </Stack>
