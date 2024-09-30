8842e634eb1ce5067c66c034c5be1c836233306f
tanaka0005 <tanaka00005.com>
2024-09-30 17:54:28 +0900

    update:ビューを整えた

--- src/components/managementPopup/PopupCard.tsx
+++ src/components/managementPopup/PopupCard.tsx
@@ -5,7 +5,7 @@ import PopupScreen from './PopupScreen';
 function PopupCard() {
   return (
     <div>
-      <Card sx={{ width:'70%', position: 'fixed' , ml:'15%' , mt:'5%' , height:'750px'}}>
+      <Card sx={{ width:'70%', position: 'fixed' , ml:'15%' , mt:'5%' ,mb:'5%', height:'750px'}}>
           <PopupScreen />
       </Card>
     </div>
