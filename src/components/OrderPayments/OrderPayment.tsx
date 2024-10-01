180656bcd1f71dce8add75c6bfd62a5fa5d34901
watagassa <k23095kk@aitech.ac.jp>
2024-10-01 18:37:08 +0900

    update:チケットの位置変更

--- src/components/OrderPayments/OrderPayment.tsx
+++ src/components/OrderPayments/OrderPayment.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-restricted-imports
 import OrderNumber from '../OrderNumber';
 import CollectedMoneyPaid from './CollectedMoneyPaid';
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import WritePaidSumMoney from './WritePaidSumMoney';
 import WriteNotEnoughMoney from './WriteNotEnoughMoney';
 import BackButton from './BackButton';
@@ -90,34 +90,37 @@ const OrderPayment = () => {
       {/* 通貨と合計金額、おつり */}
       <Box sx={{ display: { xs: 'block', sm: 'flex' } }}>
         {/* 通貨を表示 */}
-        <CollectedMoneyPaid
-          moneyCount1={moneyCount1}
-          moneyCount10={moneyCount10}
-          moneyCount100={moneyCount100}
-          moneyCount1000={moneyCount1000}
-          moneyCount10000={moneyCount10000}
-          moneyCount5={moneyCount5}
-          moneyCount50={moneyCount50}
-          moneyCount500={moneyCount500}
-          moneyCount5000={moneyCount5000}
-          setMoneyCount1={setMoneyCount1}
-          setMoneyCount10={setMoneyCount10}
-          setMoneyCount100={setMoneyCount100}
-          setMoneyCount1000={setMoneyCount1000}
-          setMoneyCount10000={setMoneyCount10000}
-          setMoneyCount5={setMoneyCount5}
-          setMoneyCount50={setMoneyCount50}
-          setMoneyCount500={setMoneyCount500}
-          setMoneyCount5000={setMoneyCount5000}
-        />
-        <Box sx={{ marginTop: { sm: '15rem', md: '10rem' } }}>
-          {/* お支払いと合計金額 */}
+        <Stack spacing="2rem" sx={{ display: 'block' }}>
+          <CollectedMoneyPaid
+            moneyCount1={moneyCount1}
+            moneyCount10={moneyCount10}
+            moneyCount100={moneyCount100}
+            moneyCount1000={moneyCount1000}
+            moneyCount10000={moneyCount10000}
+            moneyCount5={moneyCount5}
+            moneyCount50={moneyCount50}
+            moneyCount500={moneyCount500}
+            moneyCount5000={moneyCount5000}
+            setMoneyCount1={setMoneyCount1}
+            setMoneyCount10={setMoneyCount10}
+            setMoneyCount100={setMoneyCount100}
+            setMoneyCount1000={setMoneyCount1000}
+            setMoneyCount10000={setMoneyCount10000}
+            setMoneyCount5={setMoneyCount5}
+            setMoneyCount50={setMoneyCount50}
+            setMoneyCount500={setMoneyCount500}
+            setMoneyCount5000={setMoneyCount5000}
+          />
           <TiketCount
             count={tiketCount100}
             image="public/tiket_100.svg"
             setCount={setTiketCount100}
             totalAmount={getTotalAmount()}
           />
+        </Stack>
+
+        <Box sx={{ marginTop: { sm: '15rem', md: '10rem' } }}>
+          {/* お支払いと合計金額 */}
           <WritePaidSumMoney totalAmount={getTotalAmount()} totalPayment={totalPayment} />
           <Box sx={{ border: 1 }} />
           {/* おつり */}
