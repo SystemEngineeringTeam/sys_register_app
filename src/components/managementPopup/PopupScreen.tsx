b1da885a9a67dfc93e4264dd8c1d2bef922a60e3
tanaka0005 <tanaka00005.com>
2024-10-02 16:17:56 +0900

    update:タグを変えた

--- src/components/managementPopup/PopupScreen.tsx
+++ src/components/managementPopup/PopupScreen.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Card, Stack } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import CancelButton from '../CancelButton';
 import DeleteYesButton from '../DeleteYesButton';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -8,48 +8,44 @@ import { useOrderCollection } from '@/firebase/useOrderCollection';
 import { processOrderCollection } from '@/utils/processOrderCollection';
 
 
-
 const PopupScreen = () => {
-
   // const menuName = orders.map((value, index) => {
   //   const selectMenu = menu[index];
   //   console.log("selectMenu.name:",selectMenu.name);
   // })
 
+  const [selectedChangeCancel,setSelectedChangeCancel] = useState(true);
+  const [selectedChangeOkey,setSelectedChangeOkey] = useState(true);
+
   return (
     <div>
-      <Card>
-          <Stack sx={{ textAlign: 'right' }}>
-            <ClearIcon sx={{ fontSize: '80px' }} />
-          </Stack>
-          <Stack sx={{ textAlign: 'center' }}>
-            {/* fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' */}
-            <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '40px' }}>
-              本当に消去しますか？
-            </Box>
+      <Stack sx={{ textAlign: 'center' }}>
+        <Box sx={{ textAlign: 'right' }}>
+          <ClearIcon sx={{ fontSize: '80px' }} />
+        </Box>
 
-            <Stack
-              fontSize={{ xs: '20px', sm: '40px', md: '60px' }}
-              direction={'row'}
-              sx={{ justifyContent: 'center', mt: '10rem' }}
-            >
-              メニュー:唐揚げ
-            </Stack>
+        {/* fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' */}
+        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '30px' }}>
+          本当に消去しますか？
+        </Box>
 
-            <Stack fontSize={{ xs: '10px', sm: '20px', md: '30px' }} sx={{ mt: '5rem' }}>
-              全ての設定から消されます
-            </Stack>
+        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '5rem' }}>
+          メニュー:唐揚げ
+        </Box>
 
-            <Stack direction={'row'} fontSize={'40px'} sx={{mt:'5%', mb: '15%', justifyContent: 'right', mr: '7rem' }}>
-              <Button>
-                <CancelButton />
-              </Button>
-              <Button>
-                <DeleteYesButton />
-              </Button>
-            </Stack>
-          </Stack>
-      </Card>
+        <Box fontSize={{ xs: '10px', sm: '20px', md: '30px' }} sx={{ mt: '5rem' }}>
+          全ての設定から消されます
+        </Box>
+
+        <Stack direction={'row'} fontSize={'40px'} sx={{ mt: '10%', justifyContent: 'right', mr: '7rem', mb: '20px' }}>
+          <Button >
+            <CancelButton selectedChangeCancel={selectedChangeCancel} setSelectedChangeCancel={setSelectedChangeCancel}/>
+          </Button>
+          <Button>
+            <DeleteYesButton selectedChangeOkey={selectedChangeOkey} setSelectedChangeOkey={setSelectedChangeOkey}/>
+          </Button>
+        </Stack>
+      </Stack>
     </div>
   );
 };
