c77986bb4020cfb6bda9f6a9bfacddd87fdff638
tanaka0005 <tanaka00005.com>
2024-10-02 16:24:53 +0900

    add:カテゴリーを表示させるポップアップ画面

---
+++ src/components/managementPopup/CategoryDeletePopupScreen.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Box, Button, Card, Stack } from '@mui/material';
+import CancelButton from '../CancelButton';
+import DeleteYesButton from '../DeleteYesButton';
+import ClearIcon from '@mui/icons-material/Clear';
+import { processOrderChange } from '@/utils/processOrderChange';
+import { useOrderCollection } from '@/firebase/useOrderCollection';
+import { processOrderCollection } from '@/utils/processOrderCollection';
+
+const CategoryDeletePopupScreen = () => {
+  const [selectedChangeCancel, setSelectedChangeCalcel] = useState(true);
+  const [selectedChangeOkey,setSelectedChangeOkey] = useState(true);
+  return (
+    <div>
+      <Stack sx={{ textAlign: 'center' }}>
+        <Box sx={{ textAlign: 'right' }}>
+          <ClearIcon sx={{ fontSize: '80px' }} />
+        </Box>
+
+        {/* fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' */}
+        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '30px' }}>
+          本当に消去しますか？
+        </Box>
+
+        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '5rem' }}>
+          カテゴリー:フード
+        </Box>
+
+        <Box fontSize={{ xs: '10px', sm: '20px', md: '30px' }} sx={{ mt: '5rem' }}>
+          そのカテゴリーにある商品は全て削除されます
+        </Box>
+
+        <Stack direction={'row'} fontSize={'40px'} sx={{ mt: '10%', justifyContent: 'right', mr: '7rem' , mb:'20px'}}>
+          <Button>
+            <CancelButton selectedChangeCancel={selectedChangeCancel} setSelectedChangeCancel={setSelectedChangeCalcel}/>
+          </Button>
+          <Button>
+          <DeleteYesButton selectedChangeOkey={selectedChangeOkey} setSelectedChangeOkey={setSelectedChangeOkey}/>
+          </Button>
+        </Stack>
+      </Stack>
+    </div>
+  );
+};
+
+export default CategoryDeletePopupScreen;
