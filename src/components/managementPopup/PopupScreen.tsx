5c04412ef5c5511b635bd0b0e8f559a929452bab
tanaka0005 <tanaka00005.com>
2024-10-04 16:32:40 +0900

    update:れスポンシブルデザインに対応させた

--- src/components/managementPopup/PopupScreen.tsx
+++ src/components/managementPopup/PopupScreen.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Stack } from '@mui/material';
+import { Box, Button, Card, Stack, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import CancelButton from './CancelButton';
 import DeleteYesButton from './DeleteYesButton';
@@ -24,19 +24,19 @@ const PopupScreen = () => {
         </Box>
 
         {/* fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' */}
-        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '30px' }}>
-          本当に消去しますか？
+        <Box  sx={{ mt: '30px' }}>
+          <Typography sx={{ fontSize: 'clamp(1.25rem, 0.682rem + 2.42vw, 2.5rem)'}}>本当に消去しますか？</Typography>
         </Box>
 
-        <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '5rem' }}>
-          メニュー:唐揚げ
+        <Box  sx={{ mt: '5rem' }}>
+        <Typography sx={{ fontSize: 'clamp(1.4rem, 0.8rem + 3.6vw, 4.0rem)'}}>メニュー:唐揚げ</Typography>
         </Box>
 
-        <Box fontSize={{ xs: '10px', sm: '20px', md: '30px' }} sx={{ mt: '5rem' }}>
-          全ての設定から消されます
+        <Box  sx={{ mt: '5rem' }}>
+        <Typography sx={{ fontSize: 'clamp(1.0rem, 0.4rem + 2.0vw, 2.0rem)'}}>全ての設定から消されます</Typography>
         </Box>
 
-        <Stack direction={'row'} fontSize={'40px'} sx={{ mt: '10%', justifyContent: 'right', mr: '7rem', mb: '20px' }}>
+        <Stack direction={'row'}  sx={{ mt: '10%', justifyContent: 'right', mr: '7rem', mb: '20px' }}>
           <Button>
             <CancelButton
               selectedChangeCancel={selectedChangeCancel}
