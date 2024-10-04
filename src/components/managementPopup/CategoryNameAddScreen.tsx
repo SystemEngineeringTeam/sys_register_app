86633f2a2b1466c9346d1f791927270e0bb3ba2f
tanaka0005 <tanaka00005.com>
2024-10-04 16:32:20 +0900

    update:れスポンシブルデザインに対応させた

--- src/components/managementPopup/CategoryNameAddScreen.tsx
+++ src/components/managementPopup/CategoryNameAddScreen.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
 import CancelButton from './CancelButton';
@@ -17,7 +17,7 @@ function CategoryNameAddScreen() {
 
         {/* fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' */}
         <Box fontSize={{ xs: '20px', sm: '40px', md: '60px' }} sx={{ mt: '40px' }}>
-          カテゴリ追加
+        <Typography sx={{ fontSize: 'clamp(1.0rem, 0.5rem + 2.0vw, 4.2rem)' }}>カテゴリ追加</Typography>
         </Box>
 
         <Box fontSize={{ xs: '10px', sm: '20px', md: '30px' }} sx={{ mt: '5rem' }}>
