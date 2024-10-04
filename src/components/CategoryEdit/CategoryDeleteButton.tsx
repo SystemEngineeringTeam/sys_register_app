1885c37c104dac629a6c513e3d4f1e28066d3768
tanaka0005 <tanaka00005.com>
2024-10-04 18:15:51 +0900

    add:ボタンコンポーネントの追加

---
+++ src/components/CategoryEdit/CategoryDeleteButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function CategoryDeleteButton() {
+  return (
+    <div>
+        <Button sx={{bgcolor:'red' , color:'black', fontSize:'20px' , width:'10rem'}}>消去</Button>
+      
+    </div>
+  )
+}
+
+export default CategoryDeleteButton
