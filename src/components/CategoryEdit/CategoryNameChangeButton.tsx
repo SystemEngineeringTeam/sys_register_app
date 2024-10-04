ce7f89fa3b61ffea94192ba1cad8d514cc657b77
tanaka0005 <tanaka00005.com>
2024-10-04 18:15:33 +0900

    add:ボタンコンポーネントの作成

---
+++ src/components/CategoryEdit/CategoryNameChangeButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function CategoryNameChangeButton() {
+  return (
+    <div>
+        <Button sx={{bgcolor:'orange' , color:'black', fontSize:'20px',width:'10rem'}}>名称変更</Button>
+    </div>
+  )
+}
+
+export default CategoryNameChangeButton
