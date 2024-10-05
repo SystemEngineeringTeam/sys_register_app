c033ac53094c988360e4d669180e0a8c22f37ae2
watagassa <k23095kk@aitech.ac.jp>
2024-10-05 19:14:45 +0900

    add:numがNaNだった時に対応

--- src/utils/setRegisterMoney.ts
+++ src/utils/setRegisterMoney.ts
@@ -5,6 +5,11 @@ export function setMoneyFnc(
   moneyKind: 'date' | 1 | 5 | 10 | 50 | 100 | 500 | 1000 | 5000 | 10000 | 'total',
   setRegisterMoney: React.Dispatch<React.SetStateAction<money>>,
 ) {
+  // NaNが入らないようにする処理
+  let tmpNum = num;
+  if (Number.isNaN(tmpNum)) {
+    tmpNum = 0;
+  }
   switch (moneyKind) {
     case 'date':
       // eslint-disable-next-line no-case-declarations
@@ -14,34 +19,38 @@ export function setMoneyFnc(
       setRegisterMoney((prev) => ({ ...prev, date: dateTime }));
       break;
     case 'total':
-      setRegisterMoney((prev) => ({ ...prev, total: num }));
+      setRegisterMoney((prev) => ({ ...prev, total: tmpNum }));
       break;
     case 1:
-      setRegisterMoney((prev) => ({ ...prev, '1': num, total: prev.total - (prev[1] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '1': tmpNum, total: prev.total - (prev[1] - tmpNum) * moneyKind }));
       break;
     case 5:
-      setRegisterMoney((prev) => ({ ...prev, '5': num, total: prev.total - (prev[5] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '5': tmpNum, total: prev.total - (prev[5] - tmpNum) * moneyKind }));
       break;
     case 10:
-      setRegisterMoney((prev) => ({ ...prev, '10': num, total: prev.total - (prev[10] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '10': tmpNum, total: prev.total - (prev[10] - tmpNum) * moneyKind }));
       break;
     case 50:
-      setRegisterMoney((prev) => ({ ...prev, '50': num, total: prev.total - (prev[50] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '50': tmpNum, total: prev.total - (prev[50] - tmpNum) * moneyKind }));
       break;
     case 100:
-      setRegisterMoney((prev) => ({ ...prev, '100': num, total: prev.total - (prev[100] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '100': tmpNum, total: prev.total - (prev[100] - tmpNum) * moneyKind }));
       break;
     case 500:
-      setRegisterMoney((prev) => ({ ...prev, '500': num, total: prev.total - (prev[500] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '500': tmpNum, total: prev.total - (prev[500] - tmpNum) * moneyKind }));
       break;
     case 1000:
-      setRegisterMoney((prev) => ({ ...prev, '1000': num, total: prev.total - (prev[1000] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '1000': tmpNum, total: prev.total - (prev[1000] - tmpNum) * moneyKind }));
       break;
     case 5000:
-      setRegisterMoney((prev) => ({ ...prev, '5000': num, total: prev.total - (prev[5000] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({ ...prev, '5000': tmpNum, total: prev.total - (prev[5000] - tmpNum) * moneyKind }));
       break;
     case 10000:
-      setRegisterMoney((prev) => ({ ...prev, '10000': num, total: prev.total - (prev[10000] - num) * moneyKind }));
+      setRegisterMoney((prev) => ({
+        ...prev,
+        '10000': tmpNum,
+        total: prev.total - (prev[10000] - tmpNum) * moneyKind,
+      }));
       break;
     default:
   }
