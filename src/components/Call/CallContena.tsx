8a423cfcdcdcfe28210fe562d85c5f8579497efb
watagassa <k23095kk@aitech.ac.jp>
2024-10-06 19:36:18 +0900

    update:dataの読み取り回数を減らすため、dataを親コンポーネントで呼び出し

--- src/components/Call/CallContena.tsx
+++ src/components/Call/CallContena.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Divider, Stack } from '@mui/material';
+import { Box, Divider, Stack } from '@mui/material';
 import ProductCallContena from './ProductCallContena';
+import { type orderCollection } from '@/types';
 
 interface CallContenaProps {
+  data: orderCollection[] | undefined;
   call: number[];
 }
 
-const CallContena: React.FC<CallContenaProps> = ({ call }) => {
+const CallContena: React.FC<CallContenaProps> = ({ call, data }) => {
   return (
     <div>
       <Stack alignItems="center" divider={<Divider flexItem />} spacing={2}>
         {/* Dividerで区切り線を入れる */}
-        {call.map((item, index) => (
-          <ProductCallContena key={index} id={item} />
+        {call.map((orderCollectionId) => (
+          // orderCollectionIdは一意
+          <Box key={orderCollectionId}>
+            <ProductCallContena data={data} id={orderCollectionId} />
+          </Box>
         ))}
       </Stack>
     </div>
