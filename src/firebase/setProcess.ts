440fc758c0a97a2f20e8f19d2748ea3fd53eba55
watagassa <k23095kk@aitech.ac.jp>
2024-10-06 19:40:28 +0900

    update:true,false切り替えを前の値の反転に変更

--- src/firebase/setProcess.ts
+++ src/firebase/setProcess.ts
@@ -1,23 +1,26 @@
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useAtomValue } from 'jotai';
-import { userAtom } from '../login/AdminLogin';
+import { userAtom } from '@/login/AdminLogin';
 import { db } from './firebase';
+import { type orderCollection } from '@/types';
 
 export const useOrderUpdate = () => {
   const user = useAtomValue(userAtom);
 
-  if (!user) {
+  if (user == null) {
     throw new Error('User is not logged in');
   }
 
   const updateOrderStatus = async (id: string, field: string) => {
     const orderRef = doc(db, 'shop_user', user.uid, 'orderCollection', id);
-
+    const tmpField = field as 'accounting' | 'cooking' | 'offer';
+    const docSnap = await getDoc(orderRef);
+    const orderData = docSnap.data() as orderCollection;
+    const booleanField = orderData[tmpField];
     try {
       await updateDoc(orderRef, {
-        [field]: true,
+        [field]: !booleanField,
       });
-      console.log(`${field} field updated successfully for order ${id}`);
     } catch (error) {
       console.error(`Failed to update ${field} for order ${id}:`, error);
       throw new Error(`Failed to update ${field} field`);
