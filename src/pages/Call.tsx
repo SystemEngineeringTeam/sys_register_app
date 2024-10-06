576caacf91ab6c6bdafce246911b9ddd6a313b7a
watagassa <k23095kk@aitech.ac.jp>
2024-10-06 19:41:01 +0900

    update:親コンポーネントでdateを読み出し

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -1,15 +1,25 @@
-import CallContena from '../components/Call/CallContena';
-import { processOrderCollection } from '../utils/processOrderCollection';
+import CallContena from '@/components/Call/CallContena';
+import { useOrderCollection } from '@/firebase/useOrderCollection';
+import { processOrderCollection } from '@/utils/processOrderCollection';
+import { Box, Card, Stack } from '@mui/material';
 
 const Call = () => {
-  const process = 'offer';
-  const call = processOrderCollection(process);
-
-  const calls = call.map((call) => Number(call.id));
+  const orderCallsId = processOrderCollection('offer');
+  const orderCallsIdToNum = orderCallsId.map((callObj) => Number(callObj.id));
+  const finishedCallsId = processOrderCollection('finish');
+  const finishedCallsIdToNum = finishedCallsId.map((callObj) => Number(callObj.id));
+  const { data } = useOrderCollection();
   return (
-    <div>
-      <CallContena call={calls} />
-    </div>
+    <Stack>
+      <Card sx={{ backgroundColor: 'grey' }}>
+        <Box sx={{ fontSize: '2rem', margin: '1rem' }}>完了</Box>
+        <CallContena call={finishedCallsIdToNum} data={data} />
+      </Card>
+      <Card sx={{ fontSize: '2rem' }}>
+        <Box sx={{ fontSize: '2rem', margin: '1rem' }}>未完了</Box>
+        <CallContena call={orderCallsIdToNum} data={data} />
+      </Card>
+    </Stack>
   );
 };
 
