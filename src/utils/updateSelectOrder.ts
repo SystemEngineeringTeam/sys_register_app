7ae0e928c3443ac7cdc4217ef95f65fe9b3da819
nishi240masa <masa2401106@gmail.com>
2024-10-08 21:33:13 +0900

    add:updateOrderData

--- src/utils/updateSelectOrder.ts
+++ src/utils/updateSelectOrder.ts
@@ -1,17 +1,23 @@
 import { type order } from '@/types';
 
 export const updateOrderData = (newOrderData: order[], newOrder: order | undefined, setNewOrderData: any) => {
-  const newOrderDataCopy = newOrderData.slice();
-  const setData = newOrderDataCopy.map((order) => {
-    if (newOrder === undefined) {
-      console.log('newOrder is undefined');
-      return order;
-    }
-    if (order.item.id === newOrder.item.id) {
-      return newOrder;
-    }
-    return order;
-  });
-  setNewOrderData(setData);
-};
+  // newOrderと一致するorderをnewOrderDataから探す
+  const newOrderID = newOrderData.findIndex((order) => order.id === newOrder?.id);
 
+  // newOrderの型安全を保証
+  try {
+    if (newOrder === undefined) {
+      console.error('newOrderがundefinedです');
+      return;
+    }
+  } catch (e) {
+    console.error('newOrderの型が不正です');
+    return;
+  }
+
+  // newOrderDataのnewOrderID番目をnewOrderに更新
+  const setOrderData = (newOrderData[newOrderID] = newOrder);
+
+  // newOrderDataを更新
+  setNewOrderData(setOrderData);
+};
