df160d7f2140a72647a0d3cca7d45c98294ebb9e
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-08 21:26:21 +0900

    update:ポップアップで出すための処理

--- src/components/managementPopup/CategoryNameChangeCard.tsx
+++ src/components/managementPopup/CategoryNameChangeCard.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import CategoryNameChangeScreen from './CategoryNameChangeScreen';
 import { Card } from '@mui/material';
 
-const CategoryNameChangeCard = () => {
+interface CategoryNameAddCard{
+  iconClose: () => void;
+}
+
+const CategoryNameChangeCard = ({iconClose}:CategoryNameAddCard) => {
   return (
     <div>
-      <Card sx={{ width: '70%', ml: '15%', mt: '5%', height: '50%' }}>
-        <CategoryNameChangeScreen />
+      <Card sx={{}}>
+        <CategoryNameChangeScreen iconClose={iconClose}/>
       </Card>
     </div>
   );
