d771cf8c1ad843e8fd8dc92dac9a68b673f807ff
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-08 21:25:36 +0900

    update:ポップアップに追加の処理

--- src/components/managementPopup/CategoryNameAddCard.tsx
+++ src/components/managementPopup/CategoryNameAddCard.tsx
@@ -2,11 +2,15 @@ import { Card } from '@mui/material';
 import React from 'react';
 import CategoryNameAddScreen from './CategoryNameAddScreen';
 
-const CategoryNameAddCard = () => {
+interface CategoryNameAddCard{
+  iconClose: () => void;
+}
+
+const CategoryNameAddCard = ({iconClose}:CategoryNameAddCard) => {
   return (
     <div>
-      <Card sx={{ width: '70%', ml: '15%', mt: '5%', height: '50%' }}>
-        <CategoryNameAddScreen />
+      <Card>
+        <CategoryNameAddScreen iconClose={iconClose}/>
       </Card>
     </div>
   );
