da2fbe38cb2f7e364667ddf024ec3dd04f3c9995
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-08 18:24:53 +0900

    update:ポップアップの切り替えのためにロジックの追加

--- src/components/MenuEdit/ScreenChengeButton.tsx
+++ src/components/MenuEdit/ScreenChengeButton.tsx
@@ -2,17 +2,31 @@ import { ThemeProvider } from '@emotion/react';
 import { Button } from '@mui/material';
 // eslint-disable-next-line no-restricted-imports
 import { theme } from '../../themes/theme';
+import { Link } from 'react-router-dom';
 
 interface BackButtonProps {
   text: string;
   themeColor: 'categoryEdit' | 'addItem';
+  selectAdd: string;
+  selectEdit: string;
 }
 
-const ScreenChengeButton = ({ themeColor, text }: BackButtonProps) => {
+const ScreenChengeButton = ({ themeColor, text, 
+  selectAdd, selectEdit 
+}: BackButtonProps) => {
+
+const state = (
+  (text === "カテゴリー編集" ? {selectEdit} : {selectAdd})
+)
+
+
   return (
     <ThemeProvider theme={theme}>
       <Button
         color={themeColor}
+        component={Link}
+        to="/test"
+        state={{state}}
         sx={{
           display: 'flex',
           alignItems: 'center',
