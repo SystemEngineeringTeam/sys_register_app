22f1dc7553cfec7790b7e12174a7a2a7fb0a2975
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-08 21:27:09 +0900

    update:ポップアップで表示するための処理

--- src/pages/CategoryEdit.tsx
+++ src/pages/CategoryEdit.tsx
@@ -1,15 +1,39 @@
 import CategoryDeleteButton from '@/components/CategoryEdit/CategoryDeleteButton';
 import CategoryNameChangeButton from '@/components/CategoryEdit/CategoryNameChangeButton';
-import { Box, Stack, TextField } from '@mui/material';
-import React from 'react';
+import CategoryDeletePopupCard from '@/components/managementPopup/CategoryDeletePopupCard';
+import CategoryNameAddScreen from '@/components/managementPopup/CategoryNameAddScreen';
+import { Box, Button, Dialog, DialogContent, SelectChangeEvent, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CategoryEdit() {
   const menuArr = ['フード', 'ドリンク'];
 
+  // const categoryDelete = "消去";
+  // const categoryChange = "名称変更";
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {};
+  const iconClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Box sx={{ mr: '50px', textAlign: 'right', mt: '40px', mb: '90px' }}>
-        <TextField id="outlined-basic" label="カテゴリー追加" maxRows={6} multiline variant="outlined" />
+        <Button sx={{ ontSize: '20px', width: '10rem' }} variant="outlined" onClick={handleOpen}>
+          カテゴリ追加
+        </Button>
+
+        <Dialog fullWidth maxWidth="md" onClose={handleClose} open={open}>
+          <DialogContent sx={{ p: 0 }}>
+            <CategoryNameAddScreen iconClose={iconClose} />
+          </DialogContent>
+        </Dialog>
       </Box>
 
       {menuArr.map((orderName) => (
