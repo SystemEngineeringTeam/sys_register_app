b9ef532cceb833a214190696dcaec1a95540172c
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-09 15:42:32 +0900

    update:表示する値をデータベースのものに

--- src/components/CategoryEdit/CategoryDeleteButton.tsx
+++ src/components/CategoryEdit/CategoryDeleteButton.tsx
@@ -3,61 +3,40 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import CategoryDeletePopupCard from '../managementPopup/CategoryDeletePopupCard';
 
-// interface CategoryDeleteButton{
-//   iconClose: () => void;
-//   handleClose: () => void;
-//   open: boolean;
-//   handleOpen: () => void
-// }
+interface CategoryDeleteButton {
+  orderName: string;
 
-const CategoryDeleteButton = () => {
+}
+
+const CategoryDeleteButton = ({ orderName }: CategoryDeleteButton) => {
   const [selectedChange, setSelectedChange] = useState(true);
 
- // 表示の状態
- const [display, setDisplay] = useState('');
+  const [open, setOpen] = useState(false);
 
- // カテゴリーの状態
- const [category, setCategory] = useState('');
-  // カテゴリーの選択
-  const handleChange = (event: SelectChangeEvent) => {
-    setCategory(event.target.value);
+  const handleOpen = () => {
+    setOpen(true);
   };
-   // 表示の選択
-   const handleDisplayChange = (event: SelectChangeEvent) => {
-     setDisplay(event.target.value);
-   };
- 
-   const [open, setOpen] = useState(false);
- 
-   const handleOpen = () => {
-     setOpen(true);
-   };
-   const handleClose = () => {};
-   const iconClose = () => {
-     setOpen(false);
-   };
+  const handleClose = () => {};
+  const iconClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
-
       <Button
         disableElevation
         onClick={handleOpen}
         sx={{ bgcolor: selectedChange ? 'red' : 'gray', color: 'black', fontSize: '20px', width: '10rem' }}
         variant="contained"
-
       >
         消去
       </Button>
 
       <Dialog fullWidth maxWidth="md" onClose={handleClose} open={open}>
         <DialogContent sx={{ p: 0 }}>
-          <CategoryDeletePopupCard iconClose={iconClose} /> 
-          
-          
+          <CategoryDeletePopupCard iconClose={iconClose}  orderName={orderName}/>
         </DialogContent>
       </Dialog>
-
     </div>
   );
 };
