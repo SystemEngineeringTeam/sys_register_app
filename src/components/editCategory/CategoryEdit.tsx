35b4af361bbec1221cc4a22fd39765f2e7e38953
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-09 15:40:24 +0900

    add:ファイルの移動

---
+++ src/components/editCategory/CategoryEdit.tsx
@@ -0,0 +1,63 @@
+import CategoryDeleteButton from '@/components/CategoryEdit/CategoryDeleteButton';
+import CategoryNameChangeButton from '@/components/CategoryEdit/CategoryNameChangeButton';
+import CategoryDeletePopupCard from '@/components/managementPopup/CategoryDeletePopupCard';
+import CategoryNameAddCard from '@/components/managementPopup/CategoryNameAddCard';
+import CategoryNameAddScreen from '@/components/managementPopup/CategoryNameAddScreen';
+import { getCategory } from '@/firebase/useCategory';
+import { useOrderCollection } from '@/firebase/useOrderCollection';
+import { sortingOrders } from '@/utils/sortingOrders';
+import { Box, Button, Dialog, DialogContent, SelectChangeEvent, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function CategoryEdit() {
+  //const menuArr = ['フード', 'ドリンク'];
+
+  // const categoryDelete = "消去";
+  // const categoryChange = "名称変更";
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {};
+  const iconClose = () => {
+    setOpen(false);
+  };
+
+  const orderData = getCategory();
+
+  return (
+    <div>
+      <Box sx={{ mr: '50px', textAlign: 'right', mt: '40px', mb: '90px' }}>
+        <Button sx={{ fontSize: '30px', width: '20rem' ,py:'40px',  bgcolor:'orange' , color:'black'}} variant="outlined" onClick={handleOpen}>
+          カテゴリ追加
+        </Button>
+
+        <Dialog fullWidth maxWidth="md" onClose={handleClose} open={open}>
+          <DialogContent sx={{ p: 0 }}>
+            <CategoryNameAddCard iconClose={iconClose}/>
+          </DialogContent>
+        </Dialog>
+      </Box>
+
+      {orderData.category.map((orderName) => (
+        <div>
+          <Stack alignItems="center" border="solid" direction="row" height="20vh">
+            <Box sx={{ fontSize: '50px', ml: '70px' }}>{orderName.name}</Box>
+
+            <Stack direction="row" sx={{ ml: 'auto' }}>
+              <Box sx={{ mr: '10px' }}>
+                <CategoryDeleteButton orderName={orderName.name} />
+              </Box>
+              <Box sx={{ ml: 'auto', mr: '10px' }}>
+                <CategoryNameChangeButton orderName={orderName.name}/>
+              </Box>
+            </Stack>
+          </Stack>
+        </div>
+      ))}
+    </div>
+  );
+}
