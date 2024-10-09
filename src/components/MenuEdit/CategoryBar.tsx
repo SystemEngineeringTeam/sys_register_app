12ba45dec9477c574d0fd4d58cf8a97ba621c510
tanaka00005 <x23054xx@aitech.ac.jp>
2024-10-09 15:35:29 +0900

    update:/testへ値を渡すようにした

--- src/components/MenuEdit/CategoryBar.tsx
+++ src/components/MenuEdit/CategoryBar.tsx
@@ -3,23 +3,38 @@ import React from 'react';
 import { type category } from '../../types/index';
 import { Box, Button } from '@mui/material';
 import { blue, orange } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 
 interface CategoryBarProps {
   categorys: category[];
   selectcategoryId: string;
   setSelectcategoryId: React.Dispatch<React.SetStateAction<string>>;
+  selectEdit: string;
+  selectAdd: string;
 }
 // 必要な機能
 // category[]を受け取ってforEachで展開
 // categoryが選択されている時は文字の色を変える
-const CategoryBar = ({ categorys, selectcategoryId, setSelectcategoryId }: CategoryBarProps) => {
+const CategoryBar = ({ categorys, selectcategoryId, setSelectcategoryId , selectEdit , selectAdd}: CategoryBarProps) => {
+
+  const state = {
+    selectAdd: "add",
+    selectEdit: undefined
+  };
+
   return (
+
+    
     <Box sx={{ display: 'flex', alignContent: 'center' }}>
       {categorys.map((cat) => (
         <Button
           key={cat.id}
+          component={Link}
+          to="/test"
+          state={{state}}
           onClick={() => {
             setSelectcategoryId(cat.id);
+
           }}
           sx={{
             display: 'flex',
@@ -30,6 +45,7 @@ const CategoryBar = ({ categorys, selectcategoryId, setSelectcategoryId }: Categ
           }}
         >
           {cat.name}
+          
         </Button>
       ))}
     </Box>
