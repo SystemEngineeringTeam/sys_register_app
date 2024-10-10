567ccf76ef9024be87583bfa4d86a6c7da901664
nishi240masa <masa2401106@gmail.com>
2024-10-10 21:28:55 +0900

    fix: Fix bug preventing order deletion in OrderDelete component

--- src/components/CustomizeChangeRight.tsx
+++ src/components/CustomizeChangeRight.tsx
@@ -30,7 +30,7 @@ const CustomizeChangeRight = ({ selectId, newOrderData }: CustomizeChangeRightPr
 
         <Box sx={{ mt: '40px' }}>
           <Box>
-            <OrderDelete />
+            <OrderDelete  />
           </Box>
           <Box sx={{ mt: '40px' }}>
             <OkeyButton selectId={selectId} />
