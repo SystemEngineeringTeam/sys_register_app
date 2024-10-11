e7ed7a691c9616c9bf7b0511ad1d2be23a728a07
watagassa <135955629+watagassa@users.noreply.github.com>
2024-10-11 21:03:28 +0900

    fix: inport文入力

--- src/components/CategoryEdit/CategoryNameChangeButton.tsx
+++ src/components/CategoryEdit/CategoryNameChangeButton.tsx
@@ -1,6 +1,5 @@
-import { Button, Dialog, DialogContent, SelectChangeEvent } from '@mui/material';
+import { Button, Dialog, DialogContent } from '@mui/material';
 import React, { useState } from 'react';
-import CategoryDeletePopupCard from '../managementPopup/CategoryDeletePopupCard';
 import CategoryNameChangeCard from '../managementPopup/CategoryNameChangeCard';
 import { category } from '@/types';
 
