6a254850a0487abb3e07607118dd47d3b321c8d7
nishi240masa <masa2401106@gmail.com>
2024-10-11 23:29:52 +0900

    refactor: Optimize code formatting and clean up imports in menuEdit.tsx and AppLayout.tsx

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import { styled, useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -22,7 +22,41 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import UserIcon from '../login/UserIcon';
 
+interface ListItems {
+  text: string;
+  icon: React.ReactNode;
+  to: string;
+}
+
 const drawerWidth = 240;
+// listItemのリスト
+const listItems: ListItems[] = [
+  {
+    text: '注文',
+    icon: <InboxIcon />,
+    to: '/order',
+  },
+  {
+    text: '調理',
+    icon: <MailIcon />,
+    to: '/cooking',
+  },
+  {
+    text: '提供',
+    icon: <MailIcon />,
+    to: '/delivery',
+  },
+  {
+    text: '呼び出し',
+    icon: <MailIcon />,
+    to: '/call',
+  },
+  {
+    text: '管理',
+    icon: <MailIcon />,
+    to: '/admin',
+  },
+];
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
   open: boolean;
@@ -75,8 +109,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 const AppLayout = () => {
   const theme = useTheme();
-  const [open, setOpen] = React.useState<boolean | null>(null);
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -128,27 +161,19 @@ const AppLayout = () => {
           </IconButton>
         </DrawerHeader>
         <Divider />
+
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
+          {listItems.map((item) => (
+            <ListItem key={item.text} component={Link} to={item.to}>
+              <ListItemButton onClick={handleDrawerClose}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
+
         <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
       </Drawer>
       <Main open={isDrawerOpen}>
         <Outlet /> {/* ここにルートで指定されたコンテンツが表示される */}
