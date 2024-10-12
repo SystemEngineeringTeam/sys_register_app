562f27d349bc2a2b6f0c93f593e42cdf9d387777
nishi240masa <masa2401106@gmail.com>
2024-10-12 17:32:06 +0900

    fix:key追加

--- src/components/OrderPayments/ItemColor.tsx
+++ src/components/OrderPayments/ItemColor.tsx
@@ -1,8 +1,7 @@
 import { theme } from '@/themes/theme';
 import { options } from '@/types';
-import { Chip, Grid } from '@mui/material';
-import React from 'react';
-
+import { Chip, Grid, Grid2 } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 interface ItemColorProps {
   options: options[];
 }
@@ -11,9 +10,9 @@ function ItemColor({ options }: ItemColorProps) {
   //option.nameがごとに色を変更
   return (
     <div>
-      <Grid container spacing={1} sx={{alignItems:'c'}}>
+      <Grid2 container spacing={1} sx={{ alignItems: 'c' }}>
         {options.map((option) => (
-          <Grid item xs={6}>
+          <Grid item xs={6} key={uuidv4()}>
             <Chip
               size="medium"
               sx={{
@@ -31,7 +30,7 @@ function ItemColor({ options }: ItemColorProps) {
             />
           </Grid>
         ))}
-      </Grid>
+      </Grid2>
     </div>
   );
 }
