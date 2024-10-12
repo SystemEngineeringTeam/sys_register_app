03ff8867359fe1556c6010b4dadf2a7bcb7d37b8
nishi240masa <masa2401106@gmail.com>
2024-10-13 03:33:09 +0900

    feat: Improve image loading performance in CollectedChenge component

--- src/components/OrderPayments/MoneyCount/CollectedChenge.tsx
+++ src/components/OrderPayments/MoneyCount/CollectedChenge.tsx
@@ -44,20 +44,47 @@ const CollectedChenge = ({ chenge, oturiMoney, setOturiMoney, todayMoney }: Coll
     <div>
       <Box>
         <Box sx={{ display: 'flex' }}>
-          <MoneyChenge ChengeCount={oturiMoney[1]} image="/money_1.svg" />
-          <MoneyChenge ChengeCount={oturiMoney[5]} image="/money_5.svg" />
-          <MoneyChenge ChengeCount={oturiMoney[10]} image="/money_10.svg" />
+          <MoneyChenge
+            ChengeCount={oturiMoney[1]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_1.svg?alt=media&token=de4bbc5c-7da0-47da-ac91-fff8e3e8ddfb"
+          />
+          <MoneyChenge
+            ChengeCount={oturiMoney[5]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_5.svg?alt=media&token=07c1ae6c-a5fc-46fa-ba66-0a65c24c31bd"
+          />
+          <MoneyChenge
+            ChengeCount={oturiMoney[10]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_10.svg?alt=media&token=ac483c6f-54c7-42d3-a6fa-2d7b560f6241"
+          />
         </Box>
         <Box sx={{ display: 'flex' }}>
-          <MoneyChenge ChengeCount={oturiMoney[50]} image="/money_50.svg" />
-          <MoneyChenge ChengeCount={oturiMoney[100]} image="/money_100.svg" />
-          <MoneyChenge ChengeCount={oturiMoney[500]} image="/money_500.svg" />
+          <MoneyChenge
+            ChengeCount={oturiMoney[50]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_50.svg?alt=media&token=eab3f6fa-5b3d-4061-af99-0a1612f0aeaa"
+          />
+          <MoneyChenge
+            ChengeCount={oturiMoney[100]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_100.svg?alt=media&token=085aa6f7-c6af-4a81-9d87-9a196f026515"
+          />
+          <MoneyChenge
+            ChengeCount={oturiMoney[500]}
+            image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_500.svg?alt=media&token=c079fcb5-2410-48fd-9497-63e1cb04871c"
+          />
         </Box>
       </Box>
       <Box sx={{ display: 'flex', marginTop: '2rem' }}>
-        <MoneyChenge ChengeCount={oturiMoney[1000]} image="/money_1000.svg" />
-        <MoneyChenge ChengeCount={oturiMoney[5000]} image="/money_5000.svg" />
-        <MoneyChenge ChengeCount={0} image="/money_10000.svg" />
+        <MoneyChenge
+          ChengeCount={oturiMoney[1000]}
+          image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_1000.svg?alt=media&token=19afad4a-30f6-4703-a609-c32be0a84d32"
+        />
+        <MoneyChenge
+          ChengeCount={oturiMoney[5000]}
+          image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_5000.svg?alt=media&token=261012e4-95b0-4c6a-a124-ea225256edef"
+        />
+        <MoneyChenge
+          ChengeCount={0}
+          image="https://firebasestorage.googleapis.com/v0/b/sys-pay.appspot.com/o/images%2Fmoney%2Fmoney_10000.svg?alt=media&token=5d30b61d-653b-486a-9c97-e7fa5dec4834"
+        />
       </Box>
     </div>
   );
